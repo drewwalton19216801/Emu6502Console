7bb7392e64cebe05d83255b07175fffb916f6b58
drewb <drewb@RAIDER>
2022-12-06 23:46:15 -0700

    Add project files.

---
+++ Emu6502Console/CPU.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "EEPROM.h"
+#include "Memory.h"
+
+class CPU
+{
+	/**
+	* CPU Registers:
+	*	- 8-bit Accumulator (A)
+	*	- 8-bit X Register (X)
+	*	- 8-bit Y Register (Y)
+	*	- 8-bit Stack Pointer (S)
+	*	- 16-bit Program Counter (PC)
+	*	- 8-bit Status Register (P)
+	**/
+public:
+	// constructor
+	CPU();
+	
+	// destructor
+	~CPU();
+
+	// Accumulator
+	unsigned char A;
+
+	// X Register
+	unsigned char X;
+
+	// Y Register
+	unsigned char Y;
+
+	// Stack Pointer
+	unsigned char S;
+	
+	// Program Counter
+	unsigned short PC;
+
+	// Status Register
+	unsigned char P;
+};
+
